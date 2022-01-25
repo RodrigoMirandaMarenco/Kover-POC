e1f7ca81e187deb927197599e2d1b8710925d5f6
Rodrigo Miranda <rodrigo.kreloco@gmail.com>
2022-01-25 12:23:31 -0600

    ✨ Add MainViewModel class

---
+++ app/src/main/java/com/rmiranda/koverpoc/MainViewModel.kt
@@ -0,0 +1,12 @@
+package com.rmiranda.koverpoc
+
+import androidx.lifecycle.ViewModel
+
+/**
+ * Created by rodrigomiranda on 25/1/22.
+ * Applaudo Studios
+ */
+class MainViewModel : ViewModel() {
+
+    fun snackBarText() = "test"
+}
